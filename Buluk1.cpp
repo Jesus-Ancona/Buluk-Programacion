e769b7e0b782c8e27eababe355f16dfa4d62172e
Jesus-Ancona <142694735+Jesus-Ancona@users.noreply.github.com>
2023-09-29 21:01:37 -0600

    Add files via upload

--- Buluk1.cpp
+++ Buluk1.cpp
@@ -11,21 +11,27 @@ int main() {
     double distanciaDerechaGarra = 0.0;
     vector<double> avanceRobot;
     vector<double> gradosRobot;
+    int cocheagarrado = 0;
 
+    double distanciaCocheEntrega = 35.0;
+    double gradosCocheEntrega = 310.0;
+
+    cout << "Durante los 15 segundos de autónomo, el robot avanzó 25 metros y giró 130 grados" << endl;
+    
     cout << "Control del robot:" << endl;
-    cout << " - Para moverte, ingresa 'PI' (Palanca Izquierda)" << endl;
-    cout << " - Para girar, ingresa 'C' (Cruceta)" << endl;
-    cout << " - Para verificar el coche, ingresa 'Y' (Botón Y)" << endl;
-    cout << " - Para verificar la garra, ingresa 'B' (Botón B)" << endl;
-    cout << " - Para entrar a 'Pits', ingresa 'XBOX' (Botón XBOX)" << endl;
-    cout << " - Para salir, ingresa 'S' (Select)" << endl;
+    cout << " - Para moverte, ingresa 'A0' (Palanca Izquierda)" << endl;
+    cout << " - Para girar, ingresa 'POV' (Cruceta)" << endl;
+    cout << " - Para verificar el coche, ingresa '3' (Botón Y)" << endl;
+    cout << " - Para verificar la garra, ingresa '1' (Botón B)" << endl;
+    cout << " - Para entrar a 'Pits', ingresa '2' (Botón XBOX)" << endl;
+    cout << " - Para salir, ingresa '7' (Select)" << endl;
 
     while (true) {
         string accion;
         cout << "Ingresa la acción: ";
         cin >> accion;
 
-        if (accion == "PI") {
+        if (accion == "A0") {
             cout << "Ingresa distancia para moverte: ";
             double distanciaMover;
             cin >> distanciaMover;
@@ -35,7 +41,7 @@ int main() {
                 distanciaRelativa -= distancia;
             }
             cout << "Falta: " << distanciaRelativa << " m, " << gradosCoche << " grados." << endl;
-        } else if (accion == "C") {
+        } else if (accion == "POV") {
             cout << "Ingresa grados para girar: ";
             double gradosGirar;
             cin >> gradosGirar;
@@ -44,7 +50,7 @@ int main() {
                 gradosCoche -= grados;
             }
             cout << "Girando " << gradosGirar << " grados." << endl;
-        } else if (accion == "Y") {
+        } else if (accion == "3") {
             cout << "Altura Coche: -20 cm, Derecha Coche: 30 cm." << endl;
             double distanciaRelativa = distanciaCoche;
             for (const auto &distancia : avanceRobot) {
@@ -54,9 +60,9 @@ int main() {
             if (distanciaRelativa == 0.0 && gradosCoche == 0.0) {
                 cout << "Puedes entrar al modo 'a los pits'." << endl;
             }
-        } else if (accion == "B") {
+        } else if (accion == "1") {
             cout << "Altura Garra: " << alturaGarra << " cm, Derecha Garra: " << distanciaDerechaGarra << " cm." << endl;
-        } else if (accion == "XBOX") {
+        } else if (accion == "2") {
             double distanciaRelativa = distanciaCoche;
             for (const auto &distancia : avanceRobot) {
                 distanciaRelativa -= distancia;
@@ -64,34 +70,35 @@ int main() {
             if (distanciaRelativa == 0.0 && gradosCoche == 0.0) {
                 cout << "Entrando al modo 'A los Pits'" << endl;
                 while (true) {
-                    cout << " - Para mover la pinza a la derecha, ingresa 'GD' (Gatillo Derecho)" << endl;
-                    cout << " - Para mover la pinza a la izquierda, ingresa 'GI' (Gatillo Izquierdo)" << endl;
-                    cout << " - Para mover la pinza hacia abajo, ingresa 'PD' (Palanca Derecha)" << endl;
-                    cout << " - Para intentar recoger el coche, ingresa 'X' (Botón X)" << endl;
-                    cout << " - Para salir de 'Pits', ingresa 'S'" << endl;
+                    cout << " - Para mover la pinza a la derecha, ingresa 'A3' (Gatillo Derecho)" << endl;
+                    cout << " - Para mover la pinza a la izquierda, ingresa 'A2' (Gatillo Izquierdo)" << endl;
+                    cout << " - Para mover la pinza hacia abajo, ingresa 'A4' (Palanca Derecha)" << endl;
+                    cout << " - Para intentar recoger el coche, ingresa '2' (Botón X)" << endl;
+                    cout << " - Para salir de 'Pits', ingresa '7'" << endl;
 
                     string accionPits;
                     cout << "Ingresa la acción: ";
                     cout << "El coche se encuentra 30cm a la derecha y -20cm hacia abajo";
                     cin >> accionPits;
 
-                    if (accionPits == "GD") {
+                    if (accionPits == "A3") {
                         distanciaDerechaGarra += 5.0;
                         cout << "Moviendo garra 5 cm a la derecha." << endl;
-                    } else if (accionPits == "GI") {
+                    } else if (accionPits == "A2") {
                         distanciaDerechaGarra -= 5.0;
                         cout << "Moviendo garra 5 cm a la izquierda." << endl;
-                    } else if (accionPits == "PD") {
+                    } else if (accionPits == "A4") {
                         alturaGarra -= 5.0;
                         cout << "Moviendo garra 5 cm hacia abajo." << endl;
-                    } else if (accionPits == "X") {
+                    } else if (accionPits == "2") {
                         if (alturaGarra == -20.0 && distanciaDerechaGarra == 30.0) {
                             cout << "Recogiendo coche y cambiando las llantas... ¡Éxito!" << endl;
+                            cocheagarrado = 1; // Coche ha sido agarrado
                             break;
                         } else {
                             cout << "Coordenadas de garra incorrectas para recoger el coche." << endl;
                         }
-                    } else if (accionPits == "S") {
+                    } else if (accionPits == "7") {
                         cout << "Saliendo del modo 'A los Pits'." << endl;
                         break;
                     } else {
@@ -101,10 +108,59 @@ int main() {
             } else {
                 cout << "No puedes entrar al modo 'A los Pits' hasta que llegues al coche." << endl;
             }
-        } else if (accion == "S") {
+        } else if (accion == "7") {
             break;
         } else {
             cout << "Acción no reconocida" << endl;
+        }
+    }
+
+    // Modo "Entrega" después de agarrar el coche
+    while (cocheagarrado) {
+        cout << "Has agarrado el coche. Debes ir a entregarlo." << endl;
+        cout << "Distancia para entrega: " << distanciaCocheEntrega << " m, Girar: " << gradosCocheEntrega << " grados." << endl;
+        
+        string accionEntrega;
+        double distanciaMetros = 0.0;
+        double gradosGirar = 0.0;
+        
+        while (true) {
+            cout << "Ingresa la acción ('A0' para moverte, 'POV' para girar, '5' para salir): ";
+            cin >> accionEntrega;
+
+            if (accionEntrega == "A0") {
+                cout << "Ingresa distancia para moverte: ";
+                cin >> distanciaMetros;
+            } else if (accionEntrega == "POV") {
+                cout << "Ingresa grados para girar: ";
+                cin >> gradosGirar;
+            } else if (accionEntrega == "5") {
+                cout << "Saliendo del modo 'Entrega'." << endl;
+                cocheagarrado = 0; // Salir del modo "Entrega"
+                break;
+            } else {
+                cout << "Acción no reconocida" << endl;
+                continue;
+            }
+
+            distanciaCocheEntrega -= distanciaMetros;
+            gradosCocheEntrega -= gradosGirar;
+
+            cout << "Falta: " << distanciaCocheEntrega << " m, Girar: " << gradosCocheEntrega << " grados." << endl;
+
+            if (distanciaCocheEntrega <= 0.0 && gradosCocheEntrega <= 0.0) {
+                cout << "Llegaste al punto de entrega, ¿soltar coche? (0(Sí)/N(no)): ";
+                string respuesta;
+                cin >> respuesta;
+                if (respuesta == "0") {
+                    cout << "¡Coche entregado con éxito!" << endl;
+                    cocheagarrado = 0; // Marcar que el coche ha sido entregado
+                    break;
+                }
+                else if (respuesta == "N" || respuesta == "n") {
+                    break;
+                }
+            }
         }
     }
 
